eb63c5b5f818b1cf3239fc65079820e79adaf85b
Erik Anderson <efa@eecs.berkeley.edu>
2021-03-30 16:03:22 -0700

    first pass 1.4.4 tool complete

--- sourceme.sh
+++ sourceme.sh
@@ -11,10 +11,10 @@ fi
 # Set MYPYPATH accordingly
 if [ -z "$MYPYPATH" ]
 then
-    export MYPYPATH=$PWD/toolbox-freerouting-tools
+    export MYPYPATH=$PWD/toolbox_freerouting_tools
 else
-    export MYPYPATH=$PWD/toolbox-freerouting-tools:$MYPYPATH
+    export MYPYPATH=$PWD/toolbox_freerouting_tools:$MYPYPATH
 fi
 
-# Set TOOLBOX-FREEROUTING-TOOLS_HOME variable
-export TOOLBOX-FREEROUTING-TOOLS_HOME=$PWD
+# Set TOOLBOX_FREEROUTING_TOOLS_HOME variable
+export TOOLBOX_FREEROUTING_TOOLS_HOME=$PWD
---
+++ toolbox_freerouting_tools/1.4.4/1.4.4/__init__.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Erik Anderson
+# Email: erik.francis.anderson@gmail.com
+# Date: 01/26/2021
+"""Docstring for module __init__.py"""
+
+# Imports - standard library
+import os
+from typing import Callable, List
+import subprocess
+
+# Imports - 3rd party packages
+from toolbox.tool import Tool, ToolError
+from toolbox.database import Database
+from toolbox.logger import LogLevel
+from toolbox.utils import *
+from jinja2 import StrictUndefined, Environment, FileSystemLoader
+
+# Imports - local source
+from jinja_tool import JinjaTool
+
+
+class FreeRoutingTool(JinjaTool):
+    """Xilinx synthesis and implementation tool"""
+    def __init__(self, db: Database, log: Callable[[str, LogLevel], None]):
+        super(FreeRoutingTool, self).__init__(db, log)
+        self.free = self.get_db(self.get_namespace("FreeRoutingTool"))
+        self.bin = BinaryDriver(str(Path(self.free["bin"]).resolve()))
+        self.template_file = "my_rules.rules"
+        self.render_file = os.path.join(self.get_db("internal.job_dir"), self.template_file)
+
+    def steps(self) -> List[Callable[[], None]]:
+        """Returns a list of functions to run for each step"""
+        return [self.render_rules, self.run_freerouting]
+
+    def render_rules(self):
+        """Renders tcl file that vivado will run in batch mode"""
+        self.render_to_file(self.template_file, self.render_file)
+
+    def run_freerouting(self):
+        """Actually runs the freerouting command"""
+        if self.free["execute"]:
+            # Add options
+            self.bin.add_option("-de", self.free["dsn_file"])
+            self.bin.add_option("-dr", self.render_file)
+            self.bin.add_option("-do", f"output.{self.free['output_format']}")
+            self.bin.add_option("-mp", self.free["num_passes"])
+            if self.free["language"] != "english":
+                self.bin.add_option("-l", "de")
+            # Execute binary
+            self.log(self.bin.get_execute_string())
+            self.bin.execute(directory=self.get_db('internal.job_dir'))
+            self.log(
+                f"Final implementation in => {Path(self.get_db('internal.job_dir')).relative_to(self.get_db('internal.work_dir'))}"
+            )
+        else:
+            self.log(
+                "Freerouting implement execute flag set to false. Design not routed."
+            )
